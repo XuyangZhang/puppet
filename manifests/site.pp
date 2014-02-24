a965ac4e5656d65b8b77575b3611073692b044cb
XuyangZhang <xuyang.zhang@xrpension.com>
2014-02-24 19:14:00 +0800

    second

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,5 @@
 node default {
-	file { "/var/hhhhhhaaaaaaaaaaa.txt":content => "puppet cert OK!";}
+	file { "/var/hhhhhhaaaaoooaaaa.txt":content => "puppet cert OK!";}
 } 
 file { "/etc/sudoers":
         owner => root, group => root, mode => 440
